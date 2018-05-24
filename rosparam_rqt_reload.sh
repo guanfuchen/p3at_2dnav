285696b460bf677eb6e92c47c829e1328277da7e
guanfuchen <guanfuchen@zju.edu.cn>
2018-05-24 16:24:15 +0800

    add p3at nav all stack for whole launch and rqt yaml param

--- rosparam_rqt_reload.sh
+++ rosparam_rqt_reload.sh
@@ -1,5 +1,6 @@
-rosrun dynamic_reconfigure dynparam load /move_base p3at_move_base_rqt_configure.yaml
-rosrun dynamic_reconfigure dynparam load /move_base/TebLocalPlannerROS p3at_move_base_teb_rqt_configure.yaml
-rosrun dynamic_reconfigure dynparam load /move_base/global_costmap p3at_move_base_gcostmap_rqt_configure.yaml
-rosrun dynamic_reconfigure dynparam load /move_base/local_costmap p3at_move_base_lcostmap_rqt_configure.yaml
+#!/bin/bash
+rosrun dynamic_reconfigure dynparam load /move_base rqt_configure/p3at_move_base_rqt_configure.yaml
+rosrun dynamic_reconfigure dynparam load /move_base/TebLocalPlannerROS rqt_configure/p3at_move_base_teb_rqt_configure.yaml
+rosrun dynamic_reconfigure dynparam load /move_base/global_costmap rqt_configure/p3at_move_base_gcostmap_rqt_configure.yaml
+rosrun dynamic_reconfigure dynparam load /move_base/local_costmap rqt_configure/p3at_move_base_lcostmap_rqt_configure.yaml
 
